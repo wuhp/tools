92b93b687a7aec2d3813b4d03f9aa9a5afd3b721
haipeng <haipeng@mogujie.com>
2014-12-03 20:55:41 -0800

    Add utility set_ssh_no_passwd.sh
    copy_ssh_key.expect
    set_ssh_no_passwd.sh

---
+++ script/set_ssh_no_passwd.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+usage() {
+  echo "Usage: $0 ip1 ip2 ..."
+}
+
+export REMOTE_USER=root
+export REMOTE_PASSWD=hello123
+
+[ $# -eq 0 ] && usage && exit -1
+[ "$1" = "help" -o "$1" = "-h" -o "$1" = "--help" ] && usage && exit 0
+
+[ -f ~/.ssh/id_rsa ] || ssh-keygen -t rsa -f ~/.ssh/id_rsa -N "" > /dev/null
+
+which expect > /dev/null 2>&1
+[ $? -ne 0 ] && yum -y install expect
+
+for ip in $*
+do
+  echo -n "Copy ssh public key to ${ip} ...        "
+  expect -f `dirname $0`/copy_ssh_key.expect ${ip} ${REMOTE_USER} ${REMOTE_PASSWD} > /tmp/copy_ssh_key.${ip}.log 2>&1
+  [ $? -eq 0 ] && echo "[   OK   ]" || echo "[ FAILED ]"
+done
